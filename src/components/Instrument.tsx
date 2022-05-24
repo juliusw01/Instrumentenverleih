f227ca19fb93e11706b48d9280b915b3ad6d0736
juliusw01 <95189236+juliusw01@users.noreply.github.com>
2022-05-24 12:56:08 +0200

    Add files via upload

---
+++ src/components/Instrument.tsx
@@ -0,0 +1,44 @@
+import { Button, Card, CardMedia, Container, Grid } from "@mui/material";
+import ThumbUpIcon from '@mui/icons-material/ThumbUp';
+
+function Instrument(props: any){
+
+const {
+    image,
+    name
+} = props
+
+    return(
+        <Container>
+            <Card
+                sx={{
+                    bgcolor: "orange"
+                }}
+            >
+                <Grid container xs={12}>
+                    <Grid item xs={6}>
+                        <CardMedia
+                            component="img"
+                            sx={{
+                                width: "100%",
+                                alignSelf: "flex-start"
+                            }}
+                            image={image}
+                        />
+                    </Grid>
+                    <Grid item xs={3}>
+                            <p>{name}</p>
+                            <Button>
+                                Instrrumenten Knopf
+                            </Button>
+                            <Button>
+                                <ThumbUpIcon/>
+                            </Button>
+                    </Grid>
+                </Grid>
+            </Card>
+        </Container>
+    )
+
+}
+export default Instrument
